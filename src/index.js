6f96e6bfd7b8f4f467d70235c02fb97d303386ad
Mamednoor <mamed.gohabur@gmail.com>
2020-05-06 12:33:42 +0200

    change link in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import AppClass from './components/AppClass';
-import AppHook from './AppHook'
+import AppClass from './components/AppClass';
+// import AppHook from './AppHook'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppHook />
+    <AppClass />
   </React.StrictMode>,
   document.getElementById('root')
 );
