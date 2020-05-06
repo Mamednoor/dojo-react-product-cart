588de8dc3f2122b27725130ef163e6faf8902b29
Mamednoor <mamed.gohabur@gmail.com>
2020-05-06 11:11:06 +0200

    doing in class with friends' brainstorming

--- src/components/Array2.js
+++ src/components/Array2.js
@@ -1,39 +1,68 @@
 import React, { Component } from 'react'
 import '../App.css'
 
-// const initialProductList = [
-//     { id: 1, name: "produit 1", price: 50, quantity: 1 },
-//     { id: 2, name: "produit 2", price: 75, quantity: 2 },
-//     { id: 3, name: "produit 3", price: 20, quantity: 5 },
-// ];
+const initialProductList = [
+    { id: 1, name: "produit 1", price: 50, quantity: 1 },
+    { id: 2, name: "produit 2", price: 75, quantity: 2 },
+    { id: 3, name: "produit 3", price: 20, quantity: 5 },
+];
 
 
 class Array2 extends Component {
-    state = {
-        ProductList: [
-            { id: 1, name: "produit 1", price: 50, quantity: 1 },
-            { id: 2, name: "produit 2", price: 75, quantity: 2 },
-            { id: 3, name: "produit 3", price: 20, quantity: 5 },
-        ]
-    }
+    state = { initialProductList }
 
-    handleChange = (e) => {
-        let product = this.state.ProductList
-        product.forEach(item => {
-        if (item.id === parseInt(e.target.id)){ 
-            item.quantity = e.target.value}
-        // let result = 0
-        //     let result = item.quantity = e.target.value
-        //         return result,
-        this.setState({ProductList : product})
-        })}
+//     let quantite = e.target.value
+//     this.setState(prevState => ({
+//     initialProductList: prevState.initialProductList.map(
+//     product => (product.id === id ? Object.assign(product, { quantity: quantite }) : product)
+// )
+// }))
 
-    handleAdd = () => {
-        result = {items.price * items.quantity}
-    }
+
+    // handleChange = (e) => {
+    //     let product = this.state.ProductList
+    //     product.forEach(item => {
+    //     if (item.id === parseInt(e.target.id)){ 
+    //         item.quantity = e.target.value}
+    //         let quantite = e.target.value
+    //         this.setState(prevState => ({
+    //             initialProductList: prevState.initialProductList.map(
+    //             product => (product.id === id ? Object.assign(product, { quantity: quantite }) : product)
+    //         )
+    //         })}
         
+
+        // this.setState({ProductList : product})
+
+    handleChange = (id,e) => {
+        //return e.target.value
+        // console.log(e.target.value)
+        //  console.log(this.state.initialProductList[id].quantity)
+        //  console.log(...this.state.initialProductList)
+         //console.log({ ...this.state.initialProductList[id], this.state.intialProductList[id].quantity : e.target.value})
+         //this.setState({ initialProductList: { ...this.state.initialProductList[id], quantity : e.target.value}})
+        let quantite = parseInt(e.target.value)
+         //console.log(typeof quantite,quantite===0)
+        if (quantite===0) { 
+            if(window.confirm("Etes-vous sûr de bien vouloir retirer ce produit de la liste ?")){
+            this.setState(prevState => ({
+                initialProductList: prevState.initialProductList
+                .map(product => (product.id === id ? Object.assign(product, {quantity: quantite}): product))
+            }))
+            }
+        }
+    
+        else {
+            this.setState(prevState => ({
+            initialProductList: prevState.initialProductList.map(
+            product => (product.id === id ? Object.assign(product, { quantity: quantite }) : product))
+            }))
+        }
+    }
+
+
     render () {
-    const {ProductList, handleChange} = this.state
+    const {initialProductList} = this.state
     return (
         <div className="App">
             <h1>Ma commande</h1>
@@ -44,24 +73,24 @@ class Array2 extends Component {
                     <td className="td">Quantité</td>
                     <td className="td">Prix total</td>
                 </tr>
-                    {ProductList.map(items => (
-                <tr key={items.id}>
-                    <td className="td">{items.name}</td>
-                    <td className="td">{items.price} €</td>
-                    <td><input type="number" id={items.id} htmlfor="number" onChange={handleChange}/></td>                   
-                    <td className="td">{items.price * items.quantity}</td>
+                    {initialProductList.map(product => (
+                <tr key={product.id}>
+                    <td className="td">{product.name}</td>
+                    <td className="td">{product.price} €</td>
+                    <td><input type="number" id={product.id} htmlfor="number" onChange={(e) => this.handleChange(product.id,e)}/></td>                   
+                    <td className="td">{product.price * product.quantity}</td>
                     
                 </tr>
                 ))}
             </table>
-            <p>Montant de la commande :{ProductList.map(productPrice => productPrice.quantity * productPrice.price).reduce((a, b) => a + b)} €</p>
+            <p>Montant de la commande :{initialProductList.map(productPrice => productPrice.quantity * productPrice.price).reduce((a, b) => a + b)} €</p>
             <div>
                 <form>
                     <h2>Ajouter un produit</h2>
-                        <label className="field">Nom :</label>
-                        <input type="text" id="name" name="name" onChange="" value=""/>                        
-                        <label className="field">Prix :</label>
-                        <input type="number" id="number" name="prix" onChange="" value=""/> 
+                        <label >Nom :</label>
+                        <input className="field" type="text" id="name" name="name" onChange="" value="" required/>                        
+                        <label >Prix :</label>
+                        <input className="field" type="number" id="number" name="prix" onChange="" value="" required/> 
                     <input className= "button" type="submit" value="Ajouter" />
                 </form>
             </div>
