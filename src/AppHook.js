12f1da69e4fa4cd6580b1a46e2d63f3ffa75aa15
Mamednoor <mamed.gohabur@gmail.com>
2020-06-29 15:44:50 +0200

    correction and debug code

--- src/AppHook.js
+++ src/AppHook.js
@@ -1,67 +1,97 @@
-import React, { useState } from "react";
-
-import "./App.css";
+import React, {useState, useEffect} from 'react';
+import './App.css';
 
 const initialProductList = [
-  { id: 1, name: "produit 1", price: 50, quantity: 1 },
-  { id: 2, name: "produit 2", price: 75, quantity: 2 },
-  { id: 3, name: "produit 3", price: 20, quantity: 5 },
+  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
+  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
+  { id: 3, name: 'produit 3', price: 20, quantity: 5 }
 ];
 
-function App() {
-  const [product,setNewProduct] = useState(initialProductList);
+const App = () => {
+  const [productList, setProductList] = useState(initialProductList)
+  const [totalPriceList, setTotalPriceList] = useState(initialProductList.map(product => product.price * product.quantity))
+  const [newProduct, setNewProduct] = useState({id:"", name: '', price:'', quantity:1 })
 
-  const Add = e => {
-    let adding = [...product]    
-    adding.forEach(item => {
-      if(item.id === parseInt(e.target.id)){        
-        item.quantity = e.target.value
+  const addQuantity = e => {
+      const newQuantity = e.target.value
+      if (newQuantity === "0" || newQuantity === "") {
+        if (window.confirm("Etes-vous sûr de bien vouloir retirer ce produit de la liste ?")) {
+          let arr = [...productList]
+          let row = arr.find(f => f.name === e.target.name)
+          arr = arr.filter(f => f !== row)
+          setProductList(arr)
+
+        } else {
+          setProductList(productList)
+        } 
+      } else if (newQuantity !== "") {
+      let newArr = [...productList]
+      let row = newArr.find(f => f.name === e.target.name)
+      row.quantity = parseInt(newQuantity)
+      setProductList(newArr)
       }
-    })
-    setNewProduct(adding)
   }
 
+  const onChange = e => {
+    setNewProduct({...newProduct, [e.target.name]:e.target.value})
+  }
+
+  const addProduct = e => {
+    let tempIndex;
+    while (productList.map(product => product.id).includes(tempIndex)) {
+      tempIndex = Math.floor(Math.random() * 10)
+    } 
+    setNewProduct({...newProduct, id: tempIndex})
+    e.preventDefault();
+    const newArray = [...productList]
+    newArray.push(newProduct)
+    setProductList(newArray)
+  }
+
+  useEffect(()=>
+    {setTotalPriceList(productList.map(product => product.price * product.quantity))}
+    ,[productList]
+  )
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Ma commande</h1>
       <table>
-      <thead>
-        <tr>
-          <td className="td">Produit</td>
-          <td className="td">Prix unitaire</td>
-          <td className="td">Quantité</td>
-          <td className="td">Prix total</td>
-        </tr>
-      </thead>
-      <tbody>
-        {product.map(value => (
-          <tr key={value.id}>
-            <td className="td">{value.name}</td>
-            <td className="td">{value.price} €</td>
-            <td className="td">
-              <input
-                id={value.id}
-                htmlfor='number'
-                className="td"
-                onChange={Add}
-              ></input>
-            </td>
-            <td className="td">{value.quantity * value.price} €</td>
+        <thead>
+          <tr>
+            <th>Produit</th>
+            <th>Prix unitaire</th>
+            <th>Quantité</th>
+            <th>Prix total</th>
           </tr>
-        ))} 
-      </tbody>
+        </thead>
+        <tbody>
+          {productList.map(product => 
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price} €</td>
+            <td><input id={product.id} type="number" name={product.name} value={product.quantity} onChange={addQuantity}/></td>
+            <td>{product.quantity * product.price}</td>
+          </tr>)}
+        </tbody>
       </table>
-        <p>Montant de la commande :{product.map(productPrice => productPrice.quantity * productPrice.price).reduce((a, b) => a + b)} €</p>
-          <div>
-            <form>
-                <h2>Ajouter un produit</h2>
-                    <label className="field">Nom :</label>
-                    <input type="text" id="name" name="name" onChange="" value=""/>                        
-                    <label className="field">Prix :</label>
-                    <input type="number" id="name" name="prix" onChange="" value=""/> 
-                <input className= "button" type="submit" value="Ajouter" />
-            </form>
-          </div>
+          <p>Montant de la commande: 
+          <span style={{ fontWeight: 'bold' }}> 
+          {totalPriceList.reduce((accumulator, currentValue) => accumulator + currentValue)}€
+          </span></p>
+      <form onSubmit={addProduct}>
+        <h2>Ajouter un produit</h2>
+        <div className="field">
+          <label htmlFor="name">Nom</label>
+          <input type='text' id="name" name="name" onChange={onChange} value={newProduct.name} required ></input>
+        </div>
+        <div className="field">
+          <label htmlFor="price" >Prix</label>
+          <input type='number' id="price" name="price" onChange={onChange} value={newProduct.price} required ></input>
+        </div>
+        <button>Ajouter</button>
+      </form>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppClass from './components/AppClass';
-// import AppHook from './AppHook'
-import * as serviceWorker from './serviceWorker';
+// import AppClass from './components/AppClass';
+import AppHook from './AppHook'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppClass />
+    <AppHook />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
